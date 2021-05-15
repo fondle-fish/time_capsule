92c200453e099c4efbe7acdde0899467fced06f8
Lruler <1204312199@qq.com>
2021-05-15 19:07:07 +0800

    login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,7 +3,7 @@ import { View, Image, Input, Button } from '@tarojs/components'
 //import { AtButton } from 'taro-ui'
 import './index.css'
 
-export default class login extends  Component{
+export default class login extends Component {
     componentWillMount() { }
 
     componentDidMount() { }
@@ -18,7 +18,15 @@ export default class login extends  Component{
         return (
             <View className='index'>
                 <View className='border'>
-                    <View className='h'>我是华小师</View>
+                    <View className='input'>
+                        <View className='h'>我是华小师</View>
+                        <View className='input_text'>
+                            <View>校园卡号</View> <View><Input className='IDcard' type='text'></Input></View>
+                        </View>
+                        <View className='input_text'>
+                            <View>密码</View><View><Input className='IDcard' type='password'></Input></View>
+                        </View>
+                    </View>
                 </View>
                 <View className='button'>
                     <Button >确定！</Button>
