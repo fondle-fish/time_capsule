f10a3cebb38a2d42ed4944ab1dc4ce770e22d987
qingxiangying <597707165@qq.com>
2021-05-15 22:21:39 +0800

    22:21

---
+++ src/pages/manager/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { View, Image, Text, Button } from '@tarojs/components'
+//import { AtButton } from 'taro-ui'
+import wenhao from './images/问号.png'
+import './index.css'
+
+export default class login extends Component {
+    componentWillMount() { }
+
+    componentDidMount() { }
+
+    componentWillUnmount() { }
+
+    componentDidShow() { }
+
+    componentDidHide() { }
+
+    render() {
+        return (
+            <View className='index'>
+                <Image className='wenhao' src={wenhao}></Image>
+                <View className='tobury'>
+                    <Text>空空如也～<br></br></Text>
+                    <Text>快去埋下你的时间胶囊<br></br></Text>
+                    <Text>记录下专属于你的回忆吧～</Text>
+                    <Button className='button'>去埋葬我的时间胶囊</Button>
+                </View>
+
+            </View>
+        )
+    }
+}
